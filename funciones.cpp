40d21068017922875a76b3939279380e288ffd83
Pablo Rivero <88436787+prvgal@users.noreply.github.com>
2023-07-03 12:07:20 +0200

    Add files via upload

---
+++ funciones.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#include "funciones.h"
+
+void AnadirExtensiones(){
+    system("cls");
+
+    std::cout << "MENU ANADIR EXTENSIONES" << endl;
+
+    std::cout << "A continuacion, debera poner el nombre de las extensiones. Por ejemplo, jpg." << endl;
+    std::cout << "\n";
+}
