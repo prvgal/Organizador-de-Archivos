1c68b51695e0726ba24a0514551627fc0b5055e8
Pablo Rivero <88436787+prvgal@users.noreply.github.com>
2023-07-06 14:15:04 +0200

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,10 +1,12 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
-void AnadirExtensiones(void);
+size_t AnadirExtensiones(string **infoExtensiones, size_t dim);
 void ImprimirExtensiones(string *extensiones, size_t tam);
 void resize_str(string **array, size_t tamAntiguo, size_t tamNuevo);
 string IntroducirExtension(string *extensiones, size_t tamExtensiones);
 bool ComprobarExtension(string arr, string *extensiones, size_t tam);
+string *malloc_str(size_t dim);
+void free_str(string **str);
 
 #endif
