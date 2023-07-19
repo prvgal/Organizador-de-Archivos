42814180581069c5ea351b709c8ff38c28f18803
Pablo Rivero <88436787+prvgal@users.noreply.github.com>
2023-07-19 14:11:47 +0200

    Update organizar.h

--- organizar.h
+++ organizar.h
@@ -1,8 +1,11 @@
 #ifndef ORGANIZAR_H
 #define ORGANIZAR_H
 
+void CopiarArchivo(string archivoOrigen, string archivoDestino);
+void OrganizarArchivos(string* extensiones, size_t numExtensiones);
 string getRuta(void);
-short PreguntarCrearDirectorio(void);
-string AnadirBarra(const string& dir);
+string PedirRuta(int op);
+string CrearDirectorio(int op);
+int PreguntarCrearDirectorio(void);
 
 #endif
